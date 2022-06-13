dddb1bda29aff1aa4a8eee18c68dae182d2a8e68
Navid66 <navidreza668@gmail.com>
2022-06-13 12:47:08 +0600

    Adding Post Function,Manage Post
    Adding New Items Post Category

--- admin/view/add_post_view.php
+++ admin/view/add_post_view.php
@@ -1,1 +1,53 @@
+<?php
+   $categoryName = $obj->display_category();
+
+   if(isset($_POST['add_post'])){
+      $msg =$obj->add_post($_POST);
+   }
+
+?>
+
+
 <h2> Add Post Page </h2>
+<?php if (isset($msg)){echo $msg;} ?>
+<form action ="" method="POST" enctype="multipart/form-data>
+
+<div class="form-group">
+                 <label class="mb-1" for= "post_title">Post Title</label>
+              <input name="post_title" class="form-control py-4" id="post_title" type="text"/>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for= "post_content">post_content</label>
+              <textarea class="form_control" name="post_content" id="post_content" cols="30" rows="10"></textarea>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for="post_img">Upload Thumbnail</label><br>
+              <input name="post_img" class="py-4" id="post_img" type="file"/>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for= "post_category">Select Post Category</label>
+              <select class="form-control" name="post_catagory" id="post_catagory">
+                  <?php while($category=mysqli_fetch_assoc($categoryName)){ ?>
+                    <option value="<?php echo $category['cat_id'];?>"> 
+                    <?php echo $category['cat_name']; ?></option>
+                 <?php} ?>
+              </select>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for= "post_summery">Post Summery</label>
+              <input name="post_summery" class="form-control py-4" id="post_summery" type="text"/>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for= "post_tag">Post Tags</label>
+              <input name="post_tag" class="form-control py-4" id="post_tag" type="text"/>
+     </div>
+     <div class="form-group">
+                 <label class="mb-1" for= "post_status">Post Status</label>
+                 <select class="form-control" name="post_status" id="post_status">
+                    <option value="1">Published</option> 
+                    <option value="0">Unpublished</option>
+                  </select>
+     </div>
+              <input type="submit" name="add_post" value="Add Post"
+               class="form-control btn btn-block btn-primary">
+</form>
--- admin/view/manage_post_view.php
+++ admin/view/manage_post_view.php
@@ -1,1 +1,19 @@
 <h2> Manage Post Page </h2>
+<div class="table-responsive">
+  <table class="table">
+      <thead>
+          <tr>
+              <th>ID</th>
+               <th>Title</th>
+               <th>Content</th>
+               <th>Thumb</th>
+                <th>Author</th>
+                <th>Date</th>
+                <th>Category</th>
+                <th>Status</th>
+                 <th>Action</th>
+          </tr>
+      </thead>
+  </table>        
+   
+</div>
